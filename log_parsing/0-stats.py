92ad39fe9184c7e9ca221e2feae169f2eec7a7a2
koussay557 <henidkousay@gmail.com>
2024-09-30 17:20:19 +0100

    log parsing

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,40 +1,31 @@
 #!/usr/bin/python3
+"""
+Log parsing
+"""
 import sys
 
-def print_stats(total_size, status_counts):
-    """Print the accumulated metrics."""
-    print(f"File size: {total_size}")
-    for code in sorted(status_counts.keys()):
-        if status_counts[code] > 0:
-            print(f"{code}: {status_counts[code]}")
 
-total_size = 0
-status_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
-
+i = 0
+FileSize = 0
+STATUS = {'200': 0, '301': 0,
+          '400': 0, '401': 0,
+          '403': 0, '404': 0,
+          '405': 0, '500': 0}
 try:
     for line in sys.stdin:
-        parts = line.split()
-        if len(parts) < 7:
-            continue  
-        try:
-            file_size = int(parts[-1])
-            status_code = int(parts[-2])
-        except ValueError:
-            continue  
-
-        
-        total_size += file_size
-
-        if status_code in status_counts:
-            status_counts[status_code] += 1
-
-        line_count += 1
-
-        if line_count % 10 == 0:
-            print_stats(total_size, status_counts)
-
-except KeyboardInterrupt:
-    pass
+        i += 1
+        sp = line.split(' ')
+        if len(sp) > 2:
+            FileSize += int(sp[-1])
+            if sp[-2] in STATUS:
+                STATUS[sp[-2]] += 1
+        if i % 10 == 0:
+            print("File size: {}".format(FileSize))
+            for key, value in sorted(STATUS.items()):
+                if value != 0:
+                    print("{}: {}".format(key, value))
 finally:
-    print_stats(total_size, status_counts)
+    print("File size: {}".format(FileSize))
+    for key, value in sorted(STATUS.items()):
+            if value != 0:
+                print("{}: {:d}".format(key, value))
