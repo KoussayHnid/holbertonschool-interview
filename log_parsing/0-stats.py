44210f08cd3c506940bfd151d70cd0622102ff79
koussay557 <henidkousay@gmail.com>
2024-10-18 09:44:29 +0100

    update

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,31 +1,41 @@
 #!/usr/bin/python3
-"""
-Log parsing
-"""
+
+"""Log parsing"""
+
 import sys
 
+i = 0  
+FileSize = 0 
+STATUS = {'200': 0, '301': 0, '400': 0, '401': 0,
+          '403': 0, '404': 0, '405': 0, '500': 0}
 
-i = 0
-FileSize = 0
-STATUS = {'200': 0, '301': 0,
-          '400': 0, '401': 0,
-          '403': 0, '404': 0,
-          '405': 0, '500': 0}
+def print_stats(FileSize, STATUS):
+    """Prints the accumulated file size and status codes"""
+    print("File size: {}".format(FileSize))
+    for key in sorted(STATUS.keys()):
+        if STATUS[key] > 0:
+            print("{}: {}".format(key, STATUS[key]))
+
 try:
     for line in sys.stdin:
         i += 1
-        sp = line.split(' ')
+        sp = line.split()
+
         if len(sp) > 2:
-            FileSize += int(sp[-1])
+            try:
+                FileSize += int(sp[-1])
+            except (ValueError, IndexError):
+                pass
+
             if sp[-2] in STATUS:
                 STATUS[sp[-2]] += 1
+
         if i % 10 == 0:
-            print("File size: {}".format(FileSize))
-            for key, value in sorted(STATUS.items()):
-                if value != 0:
-                    print("{}: {}".format(key, value))
+            print_stats(FileSize, STATUS)
+
+except KeyboardInterrupt:
+    print_stats(FileSize, STATUS)
+    raise
+
 finally:
-    print("File size: {}".format(FileSize))
-    for key, value in sorted(STATUS.items()):
-            if value != 0:
-                print("{}: {:d}".format(key, value))
+    print_stats(FileSize, STATUS)
